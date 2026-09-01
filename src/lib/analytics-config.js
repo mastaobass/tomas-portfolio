407744ef90177322ebd8632d17fa622821c907fb
mastaobass <tomas.stonehouse@gmail.com>
2026-09-01 07:28:08 -0700

    Point PostHog at first-party proxy e.tomas-stonehouse.com

--- src/lib/analytics-config.js
+++ src/lib/analytics-config.js
@@ -1,4 +1,4 @@
 // PostHog project API key (Settings -> Project). Safe to commit.
 // It is a public frontend token, same class as a GA measurement ID.
 export const POSTHOG_KEY = "phc_szB8krkpVmGCnqgGgcyWN4jvLcrbGspd4aAJ4PyPxrFZ";
-export const POSTHOG_HOST = "https://us.i.posthog.com";
+export const POSTHOG_HOST = "https://e.tomas-stonehouse.com";
