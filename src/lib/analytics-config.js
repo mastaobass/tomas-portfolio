e046fccd7352f969c41fb02ba2a6702e54d8b5bc
mastaobass <tomas.stonehouse@gmail.com>
2026-09-01 14:47:07 -0700

    Add GA4 measurement ID G-38GJ87BDW6 alongside PostHog

--- src/lib/analytics-config.js
+++ src/lib/analytics-config.js
@@ -2,3 +2,6 @@
 // It is a public frontend token, same class as a GA measurement ID.
 export const POSTHOG_KEY = "phc_szB8krkpVmGCnqgGgcyWN4jvLcrbGspd4aAJ4PyPxrFZ";
 export const POSTHOG_HOST = "https://e.tomas-stonehouse.com";
+
+// Google Analytics 4 Measurement ID for www.tomas-stonehouse.com
+export const GA_MEASUREMENT_ID = "G-38GJ87BDW6";
